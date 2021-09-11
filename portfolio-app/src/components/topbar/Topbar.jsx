29f66aa087e975be60a62adb97d8ee24182af95f
snowMan108 <snowmanhi108@gmail.com>
2021-09-11 23:58:35 +0530

    Day - 3

--- portfolio-app/src/components/topbar/Topbar.jsx
+++ portfolio-app/src/components/topbar/Topbar.jsx
@@ -1,14 +1,31 @@
 import "./topbar.scss";
+import Person from "@material-ui/icons/Person";
+import MailIcon from "@material-ui/icons/Mail";
 
-export default function topbar() {
+export default function topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
-        <h1>Hello</h1>
         <div className="left">
-          <a href="#intro">INTRO</a>
+          <a href="#intro" className="logo">
+            logo.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+91-8248019129</span>
+          </div>
+          <div className="itemContainer">
+            <MailIcon className="icon" />
+            <span>manuvikashs@gmail.com</span>
+          </div>
         </div>
-        <div className="right">This is right</div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
