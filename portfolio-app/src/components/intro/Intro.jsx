6d7e020189d5c2fa27b75baa8934138643725c1a
snowMan108 <snowmanhi108@gmail.com>
2021-09-12 23:56:15 +0530

    Day 3 intro page with animations

--- portfolio-app/src/components/intro/Intro.jsx
+++ portfolio-app/src/components/intro/Intro.jsx
@@ -1,5 +1,37 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      strings: ["Developer", "Designer", "Content Creator"],
+      typeSpeed: 150,
+      backSpeed: 75,
+      backDelay: 1000,
+    });
+  }, []);
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/man.png" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Manuvikash</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" />
+        </a>
+      </div>
+    </div>
+  );
 }
