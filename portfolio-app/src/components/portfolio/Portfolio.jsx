d7387d0a9edb5e3f5ea2dde9ae8da3db82048716
snowMan108 <snowmanhi108@gmail.com>
2021-09-21 23:03:42 +0530

    started portfolio

--- portfolio-app/src/components/portfolio/Portfolio.jsx
+++ portfolio-app/src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,60 @@
 import "./portfolio.scss";
 
 export default function portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Moblie app</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+            alt=""
+          />
+          <h3>Banking App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
